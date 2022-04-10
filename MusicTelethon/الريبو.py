f94d60a04b76b7ca9a2a0daa99c42c754d0e0717
KAZIM MAJID <48330400+KAZIM8@users.noreply.github.com>
2022-04-10 22:06:04 +0300

    Add files via upload

---
+++ MusicTelethon/الريبو.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, SUDO_USERS
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (    ("Minggu", 60 * 60 * 24 * 7),    ("Hari", 60 * 60 * 24),    ("Jam", 60 * 60),    ("Menit", 60),    ("Detik", 1),)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+
+@Client.on_message(    filters.user(SUDO_USERS) & filters.command(["ريست"], prefixes=f"{HNDLR}"))
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("1")
+    await loli.edit("2")
+    await loli.edit("3")
+    await loli.edit("4")
+    await loli.edit("5")
+    await loli.edit("6")
+    await loli.edit("7")
+    await loli.edit("8")
+    await loli.edit("9")
+    await loli.edit("تم اعاده التشغيل[.](https://telegra.ph/file/7156094e1d7b093d32f3a.mp4)")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+@Client.on_message(filters.command(["اوامر"], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+    await m.delete()
+    HELP = f"""
+<b> hi {m.from_user.mention}!
+
+الاوامر (https://telegra.ph/file/7156094e1d7b093d32f3a.mp4)[.] Bot Music 
+
+التشغيل أغنيه : دز امر 'شغل' + اسم الاغنيه 
+التشغيل فيديو : دز امر 'شغل_فيد' + اسم الاغنيه او المقطع 
+———————————————————
+
+اذا تريد توقف مؤقت : دز امر 'استئناف'
+اذا تريد توكف الاستئناف : دز امر 'ايقاف_استئناف'
+اذا تريد توكف الاغنيه : دز امر 'اوكف او اسكت'
+
+———————————————————
+
+اذا تريد تحمل اغنيه : دز امر 'حمل '+ اسم الاغنيه
+اذا تريد فيد : دز امر 'حمل_فيد' او 'نزل فيد 
+
+
+قناة السورس : 
+
+The Channel : @hithon
+ The developer : @ytlty
+"""
+    await m.reply(HELP)
+@Client.on_message(filters.command(["سورس"], prefixes=f"{HNDLR}"))
+async def repo(client, m: Message):
+    await m.delete()
+    REPO = f"""
+<b>  hi {m.from_user.mention}!
+
+ **hithon music boots**
+
+
+The Channel : @hithon
+ The developer : @ytlty
+"""
+    await m.reply(REPO, disable_web_page_preview=True)
