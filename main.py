000f951f9e6c34173dc4aeb3d3ac27d61288a50a
KAZIM MAJID <48330400+KAZIM8@users.noreply.github.com>
2022-05-21 22:14:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from Hi.flan import arq
+from Hi.التشغيل import arq
 async def main():
     await call_py.start()
     print("""   Bot start ."""    )
