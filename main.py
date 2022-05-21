8f3f06c86e6d5fccc5f0f066359e5b5702b3c223
KAZIM MAJID <48330400+KAZIM8@users.noreply.github.com>
2022-05-21 21:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from Hi.التشغيل import arq
+from Hi.flan import arq
 async def main():
     await call_py.start()
-    print("""   البوت شغال ."""    )
+    print("""   Bot start ."""    )
     await idle()
     await arq.close()
 loop = asyncio.get_event_loop()
