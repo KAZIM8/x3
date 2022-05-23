80b5aafdfce26da13c58fa430f999b2a9b1adbb6
KAZIM MAJID <48330400+KAZIM8@users.noreply.github.com>
2022-05-23 17:17:25 +0300

    Create exampleconfig.py

---
+++ exampleconfig.py
@@ -0,0 +1,10 @@
+from sample_config import Config
+
+
+class Development(Config):
+    APP_ID = 1
+    API_HASH = "c"
+    SESSION = "i"
+    HNDLR = "."
+    SUDO_USERS = [5009120748 5263722694]
+#offub
