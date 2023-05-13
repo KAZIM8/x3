47c2152109f6b376bcdc6d74454f243ccb287baa
KAZIM <48330400+KAZIM8@users.noreply.github.com>
2023-05-13 18:55:32 +0300

    config.py

--- config.py
+++ config.py
@@ -10,19 +10,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "")
-GROUP_MODE = os.getenv("GROUP_MODE", "True")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
-contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
-)
-
-
-if GROUP_MODE == ("True" or "true"):
-    grp = True
-else:
-    grp = False
-
-GRPPLAY = grp
+contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Hi"))
 call_py = PyTgCalls(bot)
