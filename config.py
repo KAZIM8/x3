5972f484e92a8439d2ad1d59400d01c54a67f3bb
KAZIM <48330400+KAZIM8@users.noreply.github.com>
2023-05-13 18:35:10 +0300

    config.py

--- config.py
+++ config.py
@@ -2,17 +2,19 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
-
-# For Local Deploy
+# hithon
 if os.path.exists(".env"):
     load_dotenv(".env")
-
-# Necessary Vars
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+# 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "/")
+HNDLR = os.getenv("HNDLR", "")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Hi"))
+call_py = PyTgCalls(bot)
 
 
 contact_filter = filters.create(
