e14511b9410737da755f3c25ff70249be9fe2244
KAZIM <48330400+KAZIM8@users.noreply.github.com>
2023-05-13 16:47:34 +0300

    rohe l3bt w3le

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
@@ -9,11 +8,11 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "!")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+HNDLR = os.getenv("HNDLR", "/")
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
 
 contact_filter = filters.create(
@@ -21,9 +20,12 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicRioUserbot"))
+
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
+GRPPLAY = grp
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="AsadAlexaVCBot"))
 call_py = PyTgCalls(bot)
-# © 2022 GitHub, Inc.
-# Terms
-# Privacy
-# Security
