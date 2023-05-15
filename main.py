638f2f0639fa5b01ae3af865d0dd751b0feeec51
KAZIM <48330400+KAZIM8@users.noreply.github.com>
2023-05-15 22:31:22 +0300

    ksk

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import asyncio
 from pytgcalls import idle
-from config import call_py
-from Hi.التشغيل import arq
+from config import call_py, bot
+
+
 async def main():
+    print("STARTING RIOUBOT CLIENT")
+    await bot.start()
+    print("STARTING PYTGCALLS CLIENT")
     await call_py.start()
-    print("""   Bot start ."""    )
+    print(
+        """
+    ------------------------
+   | Rio Music Actived! |
+    ------------------------
+"""
+    )
     await idle()
-    await arq.close()
+    await pidle()
+    print("STOPPING USERBOT")
+    await bot.stop()
+
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
